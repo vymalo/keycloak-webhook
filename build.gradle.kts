16aa9ccf357614724fe7eaa9e3645eb9adc6a822
Stephane SEGNING LAMBOU <selastlambou@gmail.com>
2023-08-01 13:03:59 +0200

    Added support for Keycloak v22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,20 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
     id("org.openapi.generator") version "6.5.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("groovy")
 }
 
 group = "com.vymalo.keycloak.webhook"
-version = "0.2.0"
+version = "0.3.0"
 
 val gsonVersion = "2.10.1"
 val amqpVersion = "5.17.0"
 val okhttp3Version = "4.10.0"
+val okioVersion = "3.0.0"
 
 repositories {
     mavenCentral()
@@ -23,9 +25,11 @@ dependencies {
 
     api("org.apache.commons", "commons-lang3", "3.12.0")
 
-    implementation("org.keycloak", "keycloak-services", "21.0.2")
-    implementation("org.keycloak", "keycloak-server-spi", "21.0.2")
-    implementation("org.keycloak", "keycloak-server-spi-private", "21.0.2")
+    implementation(kotlin("stdlib"))
+
+    implementation("org.keycloak", "keycloak-services", "22.0.1")
+    implementation("org.keycloak", "keycloak-server-spi", "22.0.1")
+    implementation("org.keycloak", "keycloak-server-spi-private", "22.0.1")
 
     api("com.squareup.okhttp3", "okhttp", okhttp3Version)
     api("com.rabbitmq", "amqp-client", amqpVersion)
@@ -84,9 +88,12 @@ tasks {
     val shadowJar by existing(ShadowJar::class) {
         dependencies {
             include(dependency("com.squareup.okhttp3:okhttp:$okhttp3Version"))
-            include(dependency("org.jetbrains.kotlin:kotlin-reflect:1.8.0"))
+            include(dependency("com.squareup.okio:okio-jvm:$okioVersion"))
+            include(dependency("org.jetbrains.kotlin:kotlin-stdlib:1.9.0"))
+            include(dependency("org.jetbrains.kotlin:kotlin-reflect:1.9.0"))
             include(dependency("com.rabbitmq:amqp-client:$amqpVersion"))
             include(dependency("com.google.code.gson:gson:$gsonVersion"))
         }
+        dependsOn(build)
     }
 }

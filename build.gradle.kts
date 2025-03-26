d08922c8ce221d418df10ffc41107c2012592965
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-03-26 14:18:21 +0100

    chore: version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.vymalo.keycloak.webhook"
-version = "0.8.3"
+version = "0.8.4"
 
 repositories {
     mavenCentral()
--- keycloak-webhook-provider-core/src/main/kotlin/AbstractWebhookEventListenerFactory.kt
+++ keycloak-webhook-provider-core/src/main/kotlin/AbstractWebhookEventListenerFactory.kt
@@ -21,7 +21,7 @@ abstract class AbstractWebhookEventListenerFactory(
     WebhookHandler by delegate {
     private var takeList: Set<String>? = null
 
-    override fun getOperationalInfo() = mapOf("version" to "0.8.3")
+    override fun getOperationalInfo() = mapOf("version" to "0.8.4")
 
     companion object {
         @JvmStatic

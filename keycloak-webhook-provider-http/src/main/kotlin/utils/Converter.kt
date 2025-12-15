ab88470e062354039b60280e6c7f17e3cf0456e1
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-12-15 21:33:44 +0100

    feat(http): update converter to include user data fields
    Update toWebhookRequest() extension function to pass new
    user data fields (userAttributes, realmRoles, clientRoles)
    when converting WebhookPayload to WebhookRequest.

--- keycloak-webhook-provider-http/src/main/kotlin/utils/Converter.kt
+++ keycloak-webhook-provider-http/src/main/kotlin/utils/Converter.kt
@@ -4,5 +4,6 @@ import com.vymalo.keycloak.openapi.client.model.WebhookRequest
 import com.vymalo.keycloak.webhook.WebhookPayload
 
 fun WebhookPayload.toWebhookRequest() = WebhookRequest(
-    type, realmId, id, time, clientId, userId, ipAddress, error, details, resourcePath, representation
+    type, realmId, id, time, clientId, userId, ipAddress, error, details, resourcePath, representation,
+    userAttributes, realmRoles, clientRoles
 )
